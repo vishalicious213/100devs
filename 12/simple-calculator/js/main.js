31065759443302ee7b1b038dd84bb0da67add501
vishalicious213 <vishalicious213@gmail.com>
2024-08-16 12:33:44 -0700

    /12/simple-calculator: added event listener & function to js so that when #square is clicked it runs a function that squares the total and renders the result

--- 12/simple-calculator/js/main.js
+++ 12/simple-calculator/js/main.js
@@ -4,6 +4,7 @@ document.querySelector('#pumpkin').addEventListener('click', makeZero)
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector('#square').addEventListener('click', square)
 
 function makeZero() {
   total = 0
@@ -22,5 +23,10 @@ function add9() {
 
 function sub2() {
   total = total - 2
+  document.querySelector('#placeToPutResult').innerHTML = total
+}
+
+function square() {
+  total = total * total
   document.querySelector('#placeToPutResult').innerHTML = total
 }
