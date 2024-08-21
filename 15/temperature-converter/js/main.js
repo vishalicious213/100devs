9a4bc28e381d5b31d0c63a2e19d3490a5aeea43b
vishalicious213 <vishalicious213@gmail.com>
2024-08-21 12:55:46 -0700

    /15/temperature-converter: converting from f to c and rendering result

--- 15/temperature-converter/js/main.js
+++ 15/temperature-converter/js/main.js
@@ -5,9 +5,16 @@
 
 const toFButton = document.getElementById("to-fahrenheit")
 const celciusInput = document.getElementById("temp-celcius")
+const toCButton = document.getElementById("to-celcius")
+const fahrenInput = document.getElementById("temp-fahren")
 const converted = document.getElementById("converted-temp")
 
 toFButton.addEventListener("click", () => {
     const cToF = celciusInput.value * 9/5 + 32
     converted.innerText = cToF
+})
+
+toCButton.addEventListener("click", () => {
+    const fToC = fahrenInput.value - 32 * 5/9
+    converted.innerText = fToC
 })
