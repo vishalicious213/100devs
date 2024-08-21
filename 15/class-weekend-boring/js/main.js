14667e29e2eb355ad6b7a8cd2c9b4c2ba1e607d5
vishalicious213 <vishalicious213@gmail.com>
2024-08-21 12:30:34 -0700

    /15/class-weekend-boring: updated JS with lowercase conversion and added if clause for wednesday

--- 15/class-weekend-boring/js/main.js
+++ 15/class-weekend-boring/js/main.js
@@ -3,15 +3,15 @@
 document.querySelector('#check').addEventListener('click', check)
 
 function check() {
+  const day = document.querySelector('#day').value.toLowerCase()
 
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
+  if (day === "tuesday" || day === "thursday") {
     console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
+  } else if ( day === "saturday" || day === "sunday") {
+    console.log("It's the Weekend")
+  } else if (day === "wednesday") {
+    console.log("HAPPY HUMP DAY!")
+  } else {
     console.log("BORING")
   }
-
 }
