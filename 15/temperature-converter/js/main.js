a4fc3e24012a8f6632ead5cafe85427212650495
vishalicious213 <vishalicious213@gmail.com>
2024-08-21 12:46:32 -0700

    /15/temperature-converter: converting from celcius to f and logging result

--- 15/temperature-converter/js/main.js
+++ 15/temperature-converter/js/main.js
@@ -1,1 +1,12 @@
 //Write your pseduo code first! 
+// Get temperature in C
+// Convert temperature
+// Render it in the DOM
+
+const toFButton = document.getElementById("to-fahrenheit")
+const celciusInput = document.getElementById("temp-celcius")
+
+toFButton.addEventListener("click", () => {
+    const converted = celciusInput.value * 9/5 + 32
+    console.log(converted)
+})
