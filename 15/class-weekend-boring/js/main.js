203d9da21d511f4cf1e296c5a4f50b3547ddb716
vishalicious213 <vishalicious213@gmail.com>
2024-08-21 12:33:45 -0700

    /15/class-weekend-boring: updated JS to render message in DOM based on day

--- 15/class-weekend-boring/js/main.js
+++ 15/class-weekend-boring/js/main.js
@@ -4,14 +4,19 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
   const day = document.querySelector('#day').value.toLowerCase()
+  const msg = document.querySelector('#placeToSee')
 
   if (day === "tuesday" || day === "thursday") {
     console.log("YOU HAVE CLASS")
+    msg.innerText = "You have class!"
   } else if ( day === "saturday" || day === "sunday") {
     console.log("It's the Weekend")
+    msg.innerText = "It's the Weekend!"
   } else if (day === "wednesday") {
     console.log("HAPPY HUMP DAY!")
+    msg.innerText = "Happy Hump Day!"
   } else {
     console.log("BORING")
+    msg.innerText = "Nothing going on today."
   }
 }
