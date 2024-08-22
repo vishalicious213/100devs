5227175533d359b288e3c96dd184e222b1c5e4ce
vishalicious213 <vishalicious213@gmail.com>
2024-08-22 13:15:23 -0700

    /16/age-checker files: completed if...else statements

--- 16/age-checker/js/main.js
+++ 16/age-checker/js/main.js
@@ -6,6 +6,22 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
+let age
+
+if (age < 16) {
+    console.log("You cannot drive")
+} else if (age < 18) {
+    console.log("You can't hate from outside the club, because you can't even get in")
+} else if (age < 21) {
+    console.log("You can't drink")
+} else if (age < 25) {
+    console.log("You can't rent cars affordably")
+} else if (age < 30) {
+    console.log("You can't rent fancy cars affordably")
+} else {
+    console.log("There's nothing left to look forward to")
+}
+
 
 //--- Harder
 //On click of the h1
