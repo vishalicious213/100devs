a27ce8b018294874ab8e00bafb3b3c3fe0034416
vishalicious213 <vishalicious213@gmail.com>
2024-08-22 14:35:06 -0700

    /16/stop-snacking files: rendering stops in #stops

--- 16/stop-snacking/js/main.js
+++ 16/stop-snacking/js/main.js
@@ -1,1 +1,15 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+const button = document.getElementById("help")
+const stop = document.getElementById("stops")
+
+button.addEventListener("click", stopSnacking)
+
+function stopSnacking() {
+    const input = Number(document.querySelector("input").value)
+
+    for (let i = 1; i <= input; i++) {
+        console.log("STOP!")
+        stop.innerText += " STOP!"
+    }
+}
