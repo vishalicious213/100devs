c2ecd4b6b786e11d40448a93c579b6826f97409c
vishalicious213 <vishalicious213@gmail.com>
2024-08-22 13:40:20 -0700

    /16/simple-functions: completed medium problem

--- 16/simple-functions/js/main.js
+++ 16/simple-functions/js/main.js
@@ -17,6 +17,9 @@ function multiplyThree(num1, num2, num3) {
 
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+function medium(num1, num2, num3) {
+    return (num1 + num2) % num3
+}
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
