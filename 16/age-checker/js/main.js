c0a06f7e216c8e3ffa4b3ac8108d1ddd859883b0
vishalicious213 <vishalicious213@gmail.com>
2024-08-22 13:22:18 -0700

    /16/age-checker files: made output render onscreen as well as log in the console

--- 16/age-checker/js/main.js
+++ 16/age-checker/js/main.js
@@ -6,20 +6,34 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-let age
+const h1 = document.querySelector("h1")
+const input = document.querySelector("input")
+const p = document.querySelector("p")
 
-if (age < 16) {
-    console.log("You cannot drive")
-} else if (age < 18) {
-    console.log("You can't hate from outside the club, because you can't even get in")
-} else if (age < 21) {
-    console.log("You can't drink")
-} else if (age < 25) {
-    console.log("You can't rent cars affordably")
-} else if (age < 30) {
-    console.log("You can't rent fancy cars affordably")
-} else {
-    console.log("There's nothing left to look forward to")
+h1.addEventListener("click", checkAge)
+
+function checkAge() {
+    let age = Number(input.value)
+    
+    if (age < 16) {
+        console.log("You cannot drive")
+        p.textContent = "You cannot drive"
+    } else if (age < 18) {
+        console.log("You can't hate from outside the club, because you can't even get in")
+        p.textContent = "You can't hate from outside the club, because you can't even get in"
+    } else if (age < 21) {
+        console.log("You can't drink")
+        p.textContent = "You can't drink"
+    } else if (age < 25) {
+        console.log("You can't rent cars affordably")
+        p.textContent = "You can't rent cars affordably"
+    } else if (age < 30) {
+        console.log("You can't rent fancy cars affordably")
+        p.textContent = "You can't rent fancy cars affordably"
+    } else {
+        console.log("There's nothing left to look forward to")
+        p.textContent = "There's nothing left to look forward to"
+    }
 }
 
 
