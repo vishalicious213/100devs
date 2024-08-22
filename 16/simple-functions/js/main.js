86b4456f92acdbb082fb455d4c81a94351ab3615
vishalicious213 <vishalicious213@gmail.com>
2024-08-22 13:44:10 -0700

    /16/simple-functions: completed hard problem

--- 16/simple-functions/js/main.js
+++ 16/simple-functions/js/main.js
@@ -23,3 +23,19 @@ function medium(num1, num2, num3) {
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+
+function hard(num1, num2, num3, num4) {
+    let product = num1 * num2
+
+    if (product > 100) {
+        console.log(product + num3 + num4)
+    }
+
+    if (product < 100) {
+        console.log(num3 - num4)
+    }
+
+    if (product === 100) {
+        alert(num1 * num2 * num3 % num4)
+    }
+}
