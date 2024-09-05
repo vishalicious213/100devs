6b8c5d2c83174753373e5ba731026d91c9468b49
vishalicious213 <vishalicious213@gmail.com>
2024-09-05 15:30:22 -0700

    /16/arrays-fight-to-the-finish files: finished problem 1

--- 19/arrays-fight-to-the-finish/js/main.js
+++ 19/arrays-fight-to-the-finish/js/main.js
@@ -1,5 +1,13 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+const h2 = document.querySelector("h2")
 
+const movies = [
+    "Fist of the North Star",
+    "Blade Runner",
+    "Terminator 2"
+]
+
+movies.forEach(movie => h2.innerHTML += `<p>${movie}</p>`)
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
