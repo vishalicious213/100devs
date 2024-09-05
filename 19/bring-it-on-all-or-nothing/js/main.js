8efb69e7ed88d024453f0fd8072088ece3ce4b73
vishalicious213 <vishalicious213@gmail.com>
2024-09-05 12:59:25 -0700

    /19/bring-it-on-all-or-nothing: completed problem 5

--- 19/bring-it-on-all-or-nothing/js/main.js
+++ 19/bring-it-on-all-or-nothing/js/main.js
@@ -21,9 +21,19 @@ function mathPower(num1, num2) {
 }
 
 mathPower(3, 5)
+
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+function logAlert(isTrue, line) {
+    if (isTrue) {
+        alert(line)
+    } else {
+        console.log(line)
+    }
+}
 
+logAlert(true, "Don't go into your day like an accident")
+logAlert(false, "Don't repeat yourself")
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
