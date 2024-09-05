6b2e97529c989987b4a223a09dc850d1770e7759
vishalicious213 <vishalicious213@gmail.com>
2024-09-05 14:43:38 -0700

    /16/arrays-in-it-to-win-it: finished problem 3

--- 19/arrays-in-it-to-win-it/js/main.js
+++ 19/arrays-in-it-to-win-it/js/main.js
@@ -9,5 +9,6 @@ const movies = [
 let movie = movies[0]
 
 //Get the length of the original array and store it in a new variable
+let movieLength = movies.length
 
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
