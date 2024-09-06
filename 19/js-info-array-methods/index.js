266e8b8e7848ef987aa62356a587ab39362a80fe
vishalicious213 <vishalicious213@gmail.com>
2024-09-06 09:48:22 -0700

    drafted /19/js-info-array-methods/index.html

--- 19/js-info-array-methods/index.js
+++ 19/js-info-array-methods/index.js
@@ -3,3 +3,4 @@
 // 1. Translate border-left-width to borderLeftWidth
 // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
 // That is: removes all dashes, each word after dash becomes uppercased.
+console.log("test")
