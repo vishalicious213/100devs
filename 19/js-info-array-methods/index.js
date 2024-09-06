7e24fcfd4051482903d0b3bf85eff87b9f5b2f66
vishalicious213 <vishalicious213@gmail.com>
2024-09-06 11:02:50 -0700

    /19/js-info-array-methods - filter range in place completed, but it's not in-place. need to find a different solution

--- 19/js-info-array-methods/index.js
+++ 19/js-info-array-methods/index.js
@@ -32,3 +32,15 @@ function filterRange(arr, a, b) {
 }
 
 console.log(filterRange([5, 3, 8, 1], 1, 4))
+
+
+// 3. Filter range "in place"
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+// The function should only modify the array. It should not return anything.
+
+function filterRangeInPlace(arr, a, b) {
+    console.log(arr.filter(num => num >= a && num <= b))
+    console.log(arr)
+}
+
+filterRangeInPlace([5, 3, 8, 1], 1, 4)
