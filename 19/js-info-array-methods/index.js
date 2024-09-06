7ada9dd6af55367241eaef7b31e7c9f6f299aa2c
vishalicious213 <vishalicious213@gmail.com>
2024-09-06 10:35:16 -0700

    C:/Program Files/Git/19/js-info-array-methods - filter range completed

--- 19/js-info-array-methods/index.js
+++ 19/js-info-array-methods/index.js
@@ -21,3 +21,14 @@ function camelize(str) {
 camelize("background-color")
 camelize("list-style-image")
 camelize("-webkit-transition")
+
+
+// 2. Filter range
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+// The function should not modify the array. It should return the new array.
+
+function filterRange(arr, a, b) {
+    return arr.filter(num => num >= a && num <= b)
+}
+
+console.log(filterRange([5, 3, 8, 1], 1, 4))
