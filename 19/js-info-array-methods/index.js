ff3bdb99129ebd92fd7d93ac842b06feb7d32fa1
vishalicious213 <vishalicious213@gmail.com>
2024-09-06 10:10:35 -0700

    /19/js-info-array-methods - camelize: splitting string into array of words. capitalizing first letter of each word. have to deal with beginning dash

--- 19/js-info-array-methods/index.js
+++ 19/js-info-array-methods/index.js
@@ -3,4 +3,22 @@
 // 1. Translate border-left-width to borderLeftWidth
 // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
 // That is: removes all dashes, each word after dash becomes uppercased.
-console.log("test")
+
+function camelize(str) {
+    // make array of words, split on the dashes
+    let words = str.split("-")
+    console.log(words)
+
+    // capitalize words starting from 2nd word
+    // nums.forEach((num, i) => {nums[i] = num + 3})
+    words.forEach((word, i) => {
+        words[i] = word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    console.log(words)
+
+    // join words back into string
+}
+
+camelize("background-color")
+camelize("list-style-image")
+camelize("-webkit-transition")
