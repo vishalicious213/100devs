0a071053e91d939160e7e40ccaf0ab18c4586722
vishalicious213 <vishalicious213@gmail.com>
2024-09-08 11:15:20 -0700

    C:/Program Files/Git/19/js-info-array-methods - completed filter unique array members

--- 19/js-info-array-methods/index.js
+++ 19/js-info-array-methods/index.js
@@ -147,3 +147,16 @@ function getAverageAge(users) {
 }
 
 console.log( getAverageAge(ageArr) )
+
+
+// 12. Filter unique array members
+//  Let arr be an array.
+//  Create a function unique(arr) that should return an array with unique items of arr.
+
+function unique(arr) {
+    return arr.filter((item, i) => arr.indexOf(item) === i)
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+console.log( unique(strings) ); // Hare, Krishna, :-O
