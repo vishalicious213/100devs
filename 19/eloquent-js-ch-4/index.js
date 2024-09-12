c2835b4abf4bb11d4fa22e5d428a70d8dd7cbbc7
vishalicious213 <vishalicious213@gmail.com>
2024-09-12 13:02:41 -0700

    /19/eloquent-js-ch-4: added range()

--- 19/eloquent-js-ch-4/index.js
+++ 19/eloquent-js-ch-4/index.js
@@ -1,1 +1,17 @@
 console.log("Eloquent JavaScript")
+
+// 1. The Sum of a Range
+// console.log(sum(range(1, 10)));
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to and including end.
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+function range(start, end) {
+    let nums = []
+
+    for (let i = start; i <= end; i++) {
+        nums.push(i)
+        console.log(nums)
+    }
+}
+
+range(2, 5)
