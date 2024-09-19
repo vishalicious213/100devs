590c728873c375c391c64c9d466c706b49eacbfb
vishalicious213 <vishalicious213@gmail.com>
2024-09-19 14:16:15 -0700

    /22/turbo-review: drafted playerChoice()

--- 22/turbo-review/js/main.js
+++ 22/turbo-review/js/main.js
@@ -21,5 +21,22 @@ function rockPaperScissors() {
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
 
+function checkWin(playerChoice) {
+    let botChoice = rockPaperScissors()
+
+    if ( (playerChoice === "rock" && botChoice === "scissors") ||
+         (playerChoice === "paper" && botChoice === "rock" ) ||
+         (playerChoice === "scissors" && botChoice === "paper")
+    ) {
+        console.log("You win!")
+    } else if (playerChoice === botChoice) {
+        console.log("You tied!")
+    } else {
+        console.log("You lose!")
+    }
+}
+
+// checkWin("rock")
+
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
