3e336eb2b921670d6e8845cfe727f91a3659da77
vishalicious213 <vishalicious213@gmail.com>
2024-09-19 20:09:04 -0700

    /22/street-fighter: added StreetFighter constructor

--- 22/objects-street-fighter/js/main.js
+++ 22/objects-street-fighter/js/main.js
@@ -1,1 +1,20 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(SFCountry, SFHeight, SFWeight, SFGender) {
+    this.country = SFCountry
+    this.height = SFHeight
+    this.weight = SFWeight
+    this.gender = SFGender
+
+    this.roundhouse = function() {
+        console.log("Roundhouse kick")
+    }
+
+    this.fierce = function() {
+        console.log("Fierce punch")
+    }
+
+    this.throw = function() {
+        console.log("Throw")
+    }
+}
