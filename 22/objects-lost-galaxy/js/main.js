a5fb29a5ca99fe761576bbb8fc34f3ebdfd89ff8
vishalicious213 <vishalicious213@gmail.com>
2024-09-19 19:39:10 -0700

    /22/lost-galaxy: added mouse object

--- 22/objects-lost-galaxy/js/main.js
+++ 22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
 //Create a mouse object that has four properties and three methods
+
+const mouse = {}
+
+mouse.brand = "Logitech"
+mouse.color = "black"
+mouse.model = "MX Ergo"
+mouse.wireless = true
+
+mouse.leftClick = function() {
+    console.log("Left click")
+}
+
+mouse.rightClick = function() {
+    console.log("Right click")
+}
+
+mouse.scroll = function() {
+    console.log("Jump")
+}
