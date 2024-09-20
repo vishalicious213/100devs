58b0c8294258f4c82421ca0d051530454da86c46
vishalicious213 <vishalicious213@gmail.com>
2024-09-19 19:41:38 -0700

    /22/lost-galaxy: added mouse3 object

--- 22/objects-lost-galaxy/js/main.js
+++ 22/objects-lost-galaxy/js/main.js
@@ -37,3 +37,22 @@ mouse2.rightClick = function() {
 mouse2.scroll = function() {
     console.log("Jump")
 }
+
+const mouse3 = {}
+
+mouse3.brand = "Apple"
+mouse3.color = "silver"
+mouse3.model = "MAgic Trackpad 2"
+mouse3.wireless = true
+
+mouse3.leftClick = function() {
+    console.log("Left click")
+}
+
+mouse3.rightClick = function() {
+    console.log("Right click")
+}
+
+mouse3.scroll = function() {
+    console.log("Slide two fingers")
+}
