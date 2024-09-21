44773096aaebfdb58aeca4ec7fec0a9600e50df2
vishalicious213 <vishalicious213@gmail.com>
2024-09-21 12:58:07 -0700

    drafted /22/stopwatch index.js

---
+++ 22/stopwatch/index.js
@@ -0,0 +1,12 @@
+function Stopwatch() {
+    let startTime
+    let endTime
+    let running
+    let duration = 0
+
+    this.start = function() {}
+
+    this.stop = function() {}
+
+    this.reset = function() {}
+}
