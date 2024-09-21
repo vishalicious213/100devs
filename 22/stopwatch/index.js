6428f95fec26a45dd7feadbf6113cd4a282f3d53
vishalicious213 <vishalicious213@gmail.com>
2024-09-21 14:22:25 -0700

    /22/stopwatch: built out start()

--- 22/stopwatch/index.js
+++ 22/stopwatch/index.js
@@ -1,10 +1,19 @@
 function Stopwatch() {
+    // These are private variables because they don't have "this" to open them publicly
     let startTime
     let endTime
     let running
     let duration = 0
 
-    this.start = function() {}
+    this.start = function() {
+        if (running) {
+            throw new Error("Stopwatch has already started")
+        }
+
+        running = true
+
+        startTime = new Date()
+    }
 
     this.stop = function() {}
 
