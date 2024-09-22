7026a12359a1a47daa2f5a632cb3c345fef856cd
vishalicious213 <vishalicious213@gmail.com>
2024-09-22 15:00:00 -0700

    /23/turbo-review: finished #1

--- 23/turbo-review/js/main.js
+++ 23/turbo-review/js/main.js
@@ -1,6 +1,9 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
 
+const str = "Is this the best week ever?"
+alert(str.endsWith("?"))
+
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
 
 // *Functions*
