f7c5c452bd0de844fb46d277bdda3417f921e591
vishalicious213 <vishalicious213@gmail.com>
2024-09-25 13:28:44 -0700

    /23/objects-lost-galaxy: built out dog object using dot notation

--- 23/objects-lost-galaxy/js/main.js
+++ 23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a dog object that has four properties and three methods
+const dog = {}
+
+dog.breeed = "Husky"
+dog.color = "Spotted white"
+dog.legs = 4
+dog.name = "Al"
+
+dog.bark = function() {
+    console.log("Bark!")
+}
+dog.jump = function() {
+    console.log("Blast off!")
+}
+dog.fetch = function() {
+    console.log("Zoooom!")
+}
