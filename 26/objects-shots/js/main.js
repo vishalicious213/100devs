2eebb6b06a91b1c79a34750a6b980395947549a9
vishalicious213 <vishalicious213@gmail.com>
2024-10-14 15:57:49 -0700

    added 2nd search option, gave IDs to that and original. added event listener on container (menu)

--- 26/objects-shots/js/main.js
+++ 26/objects-shots/js/main.js
@@ -1,6 +1,13 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+const menu = document.getElementById("menu")
+menu.addEventListener("click", testClick)
+
 document.querySelector("button").addEventListener("click", getDrink)
 let drinksSection = document.getElementById("drinks")
+
+function testClick(e) {
+    console.log(e.target.id)
+}
 
 function getDrink() {
     let cocktail = document.querySelector("input").value
