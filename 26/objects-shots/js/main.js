cd8de8bee705e0584230ea977f604aa50e28c5e4
vishalicious213 <vishalicious213@gmail.com>
2024-10-13 22:52:02 -0700

    /26/objects-shots: rendering multiple drinks based on search

--- 26/objects-shots/js/main.js
+++ 26/objects-shots/js/main.js
@@ -1,16 +1,28 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector("button").addEventListener("click", getDrink)
+let drinksSection = document.getElementById("drinks")
 
 function getDrink() {
-    let drink = document.querySelector("input").value
+    let cocktail = document.querySelector("input").value
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks)
-        document.querySelector("h2").innerText = data.drinks[0].strDrink
-        document.querySelector("img").src = data.drinks[0].strDrinkThumb
-        document.querySelector("h3").innerText = data.drinks[0].strInstructions
+        let drinks = data.drinks
+        drinksSection.innerHTML = ""
+
+        let renderDrinks = drinks.map(drink => `
+            <h2>${drink.strDrink}</h2>
+            <p>${drink.strGlass}</p>
+            <p>${drink.strInstructions}</p>
+            <img src=${drink.strDrinkThumb} alt=${drink.strDrink}>
+        `)
+
+        drinksSection.innerHTML = renderDrinks
+        // document.querySelector("h2").innerText = data.drinks[0].strDrink
+        // document.querySelector("img").src = data.drinks[0].strDrinkThumb
+        // document.querySelector("h3").innerText = data.drinks[0].strInstructions
     })
     .catch(err => {
         console.log(`error ${err}`)
