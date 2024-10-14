498b7e0fe55174f84228d7017aa69a1db22650c2
vishalicious213 <vishalicious213@gmail.com>
2024-10-14 00:52:30 -0700

    /26/objects-shots: cleaned up html. added left-margin to h1 and input

--- 26/objects-shots/js/main.js
+++ 26/objects-shots/js/main.js
@@ -8,12 +8,7 @@ function getDrink() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.drinks)
         renderDrinks(data.drinks)
-        
-        // document.querySelector("h2").innerText = data.drinks[0].strDrink
-        // document.querySelector("img").src = data.drinks[0].strDrinkThumb
-        // document.querySelector("h3").innerText = data.drinks[0].strInstructions
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -29,7 +24,6 @@ function renderDrinks(arr) {
 
         for (let i = 1; i <= 15; i++) {
             if (drink[`strIngredient${i}`] !== null) {
-                // console.log(drink[`strMeasure${i}`], drink[`strIngredient${i}`])
                 if (drink[`strMeasure${i}`] === null) {
                     drink[`strMeasure${i}`] = ""
                 }
