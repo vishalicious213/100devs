5a1f628f4c07a37d8094c9a58208594fea2bf68f
vishalicious213 <vishalicious213@gmail.com>
2024-12-17 12:57:20 -0800

    uninstalled dotenv and removed .gitignore, package.json

--- 27/objects-nasa/js/main.js
+++ 27/objects-nasa/js/main.js
@@ -1,1 +1,18 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+require("dotenv").config()
+document.querySelector("button").addEventListener("click", getFetch)
+
+function getFetch() {
+    const choice = document.querySelector("input").value.toLowerCase()
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
