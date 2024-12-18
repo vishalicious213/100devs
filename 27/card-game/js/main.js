1992ada88efeab434f07e0d728bbdb259fdf7832
vishalicious213 <vishalicious213@gmail.com>
2024-12-18 13:17:56 -0800

    /27: cleaned up code

--- 27/card-game/js/main.js
+++ 27/card-game/js/main.js
@@ -3,28 +3,26 @@ let deckId = ""
 document.querySelector('button').addEventListener('click', drawTwo)
 
 function getDeckId() {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    deckId = data.deck_id
-  })
-  .catch(err => {
+  fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      deckId = data.deck_id
+    })
+    .catch(err => {
       console.log(`error ${err}`)
-  })
+    })
 }
 
 function drawTwo(){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
-
-  fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      })
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
 }
 
 getDeckId()
