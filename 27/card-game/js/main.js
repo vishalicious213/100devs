2de8b34282a588e09f6eef41d51ebd1dccd0e586
vishalicious213 <vishalicious213@gmail.com>
2024-12-18 13:33:58 -0800

    /27: comparing card values and displaying who won. only works for cards with numeric values, not face cards

--- 27/card-game/js/main.js
+++ 27/card-game/js/main.js
@@ -21,6 +21,14 @@ function drawTwo(){
       console.log(data)
       document.querySelector("#player1").src = data.cards[0].image
       document.querySelector("#player2").src = data.cards[1].image
+      let player1Val = Number(data.cards[0].value)
+      let player2Val = Number(data.cards[1].value)
+
+      if (player1Val > player2Val) {
+        document.querySelector("h3").innerText = `Player 1 Wins`
+      } else if (player1Val < player2Val) {
+        document.querySelector("h3").innerText = `Player 2 Wins`
+      } else document.querySelector("h3").innerText = `Time for WAR!`
     })
     .catch(err => {
       console.log(`error ${err}`)
