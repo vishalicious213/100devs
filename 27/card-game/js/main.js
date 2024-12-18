7323b43d3b4d1232d5680308f9b7f3b9b50f595e
vishalicious213 <vishalicious213@gmail.com>
2024-12-18 13:15:18 -0800

    /27: renamed getFetch() to drawTwo. getting two cards on button click

--- 27/card-game/js/main.js
+++ 27/card-game/js/main.js
@@ -1,24 +1,24 @@
 let deckId = ""
 
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', drawTwo)
 
 function getDeckId() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
   .then(res => res.json())
   .then(data => {
     console.log(data)
+    deckId = data.deck_id
   })
   .catch(err => {
       console.log(`error ${err}`)
   })
 }
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function drawTwo(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => res.json())
       .then(data => {
         console.log(data)
       })
