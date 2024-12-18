a5eb219cce471b57e3b81a1860e3304aed589202
vishalicious213 <vishalicious213@gmail.com>
2024-12-18 13:04:55 -0800

    /27: added button. added getDeckId(). getting new deck on page load

--- 27/card-game/js/main.js
+++ 27/card-game/js/main.js
@@ -1,5 +1,17 @@
-//Example fetch using pokemonapi.co
+let deckId = ""
+
 document.querySelector('button').addEventListener('click', getFetch)
+
+function getDeckId() {
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  })
+}
 
 function getFetch(){
   const choice = document.querySelector('input').value
@@ -12,5 +24,7 @@ function getFetch(){
       })
       .catch(err => {
           console.log(`error ${err}`)
-      });
+      })
 }
+
+getDeckId()
