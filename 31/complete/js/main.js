ddbffad273e124b3b319a503c7c8a9fad32ac600
vishalicious213 <vishalicious213@gmail.com>
2025-03-25 12:43:04 -0700

    /31/complete: added Cow class

--- 31/complete/js/main.js
+++ 31/complete/js/main.js
@@ -2,45 +2,65 @@ class Animal{
     constructor(name){
         this._name = name
     }
+
     get name(){
         return this._name
     }
+
     speak(){
         console.log(`${this._name} makes a sound`)
     }
 }
+
 class Dog extends Animal{
     constructor(name,breed){
         super(name)
         this._breed = breed
     }
+
     get breed(){
         return this._breed
     }
+
     speak(){
         super.speak()
         console.log(`${this.name} barks`)
     }    
 }
+
 class Cat extends Animal{
     constructor(name,breed){
         super(name)
         this._breed = breed
     }
+
     get breed(){
         return this._breed
     }
+
     speak(){
         super.speak()
         console.log(`${this.name} meows`)
     }    
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
+class Cow extends Animal{
+    constructor(name) {
+        super(name)
+    }
 
-let farm = [simba,machi,salem]
+    speak() {
+        super.speak()
+        console.log(`${this.name} moos`)
+    }
+}
+
+let simba = new Dog('Simba', 'Shepard')
+let machi = new Dog('The Machine', 'Pitbull')
+let salem = new Cat('Salem', 'American Shorthair')
+let bella = new Cow('Bella')
+
+let farm = [simba, machi, salem, bella]
 
 for( a of farm ){
     a.speak()
