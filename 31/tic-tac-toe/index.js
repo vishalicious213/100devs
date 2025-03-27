452ae77d7edfda9a9f7fdf0ac89d053d8b983c19
vishalicious213 <vishalicious213@gmail.com>
2025-03-26 21:13:04 -0700

    removed all console.logs

--- 31/tic-tac-toe/index.js
+++ 31/tic-tac-toe/index.js
@@ -5,9 +5,6 @@ let winner = ""
 let board = ["","","","","","","","",""]
 
 function makeMove(event) {
-    console.log(event.target.id, move)
-    console.log(event.target.innerText)
-
     if (winner !== "") {
         return
     }
@@ -28,71 +25,53 @@ function makeMove(event) {
 }
 
 function score() {
-    console.log(board)
-
     // X horizontal
     if (board[0] === "x" && board[1] === "x" && board[2] === "x") {
-        console.log("X wins!")
         winner = "x"
     } else if (board[3] === "x" && board[4] === "x" && board[5] === "x") {
-        console.log("X wins!")
         winner = "x"
     } else if (board[6] === "x" && board[7] === "x" && board[8] === "x") {
-        console.log("X wins!")
         winner = "x"
     }
 
     // X vertical
     if (board[0] === "x" && board[3] === "x" && board[6] === "x") {
-        console.log("X wins!")
         winner = "x"
     } else if (board[1] === "x" && board[4] === "x" && board[7] === "x") {
-        console.log("X wins!")
         winner = "x"
     } else if (board[2] === "x" && board[5] === "x" && board[8] === "x") {
-        console.log("X wins!")
         winner = "x"
     }
 
     // X diagonal
     if (board[0] === "x" && board[4] === "x" && board[8] === "x") {
-        console.log("X wins!")
         winner = "x"
     } else if (board[2] === "x" && board[4] === "x" && board[6] === "x") {
-        console.log("X wins!")
         winner = "x"
     }
 
     // O horizontal
     if (board[0] === "o" && board[1] === "o" && board[2] === "o") {
-        console.log("O wins!")
         winner = "o"
     } else if (board[3] === "o" && board[4] === "o" && board[5] === "o") {
-        console.log("O wins!")
         winner = "o"
     } else if (board[6] === "o" && board[7] === "o" && board[8] === "o") {
-        console.log("O wins!")
         winner = "o"
     }
 
     // O vertical
     if (board[0] === "o" && board[3] === "o" && board[6] === "o") {
-        console.log("O wins!")
         winner = "o"
     } else if (board[1] === "o" && board[4] === "o" && board[7] === "o") {
-        console.log("O wins!")
         winner = "o"
     } else if (board[2] === "o" && board[5] === "o" && board[8] === "o") {
-        console.log("O wins!")
         winner = "o"
     }
 
     // O diagonal
     if (board[0] === "o" && board[4] === "o" && board[8] === "o") {
-        console.log("O wins!")
         winner = "o"
     } else if (board[2] === "o" && board[4] === "o" && board[6] === "o") {
-        console.log("O wins!")
         winner = "o"
     }
 
