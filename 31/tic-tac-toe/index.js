492b054401af4865b678d1e1ec645a9016b347de
vishalicious213 <vishalicious213@gmail.com>
2025-03-26 20:18:49 -0700

    renamed play() to makeMove(). it makes sure that the clicked tile is empty & then places an X or an O depending on if the move is even or odd then increments the move #

--- 31/tic-tac-toe/index.js
+++ 31/tic-tac-toe/index.js
@@ -1,5 +1,18 @@
 console.log("Tic Tac Toe")
 
-function play(event) {
-    console.log(event.target.id)
+let move = 0
+
+function makeMove(event) {
+    console.log(event.target.id, move)
+    console.log(event.target.innerText)
+
+    if (event.target.innerText === "") {
+        move++
+
+        if (move % 2 === 0) {
+            event.target.innerText = "o"
+        } else {
+            event.target.innerText = "x"
+        }
+    }
 }
