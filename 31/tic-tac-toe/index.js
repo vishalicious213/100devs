d59e1a1e01d0c3a530705d934b8117399535f95d
vishalicious213 <vishalicious213@gmail.com>
2025-03-26 20:24:03 -0700

    added board array & score(). score() updates array with X or O at clicked index -1

--- 31/tic-tac-toe/index.js
+++ 31/tic-tac-toe/index.js
@@ -1,6 +1,7 @@
 console.log("Tic Tac Toe")
 
 let move = 0
+let board = ["","","","","","","","",""]
 
 function makeMove(event) {
     console.log(event.target.id, move)
@@ -11,8 +12,16 @@ function makeMove(event) {
 
         if (move % 2 === 0) {
             event.target.innerText = "o"
+            board[event.target.id - 1] = "o"
         } else {
             event.target.innerText = "x"
+            board[event.target.id - 1] = "x"
         }
     }
+    
+    score()
+}
+
+function score() {
+    console.log(board)
 }
