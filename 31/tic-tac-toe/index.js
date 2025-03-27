e0ee67ea5ffa8d10a2fe412ac431dd0a551bd4fc
vishalicious213 <vishalicious213@gmail.com>
2025-03-26 20:31:07 -0700

    updated score() with horizontal win logic

--- 31/tic-tac-toe/index.js
+++ 31/tic-tac-toe/index.js
@@ -24,4 +24,23 @@ function makeMove(event) {
 
 function score() {
     console.log(board)
+
+    // horizontal
+    if (board[0] === "x" && board[1] === "x" && board[2] === "x") {
+        console.log("X wins!")
+    } else if (board[3] === "x" && board[4] === "x" && board[5] === "x") {
+        console.log("X wins!")
+    } else if (board[6] === "x" && board[7] === "x" && board[8] === "x") {
+        console.log("X wins!")
+    }
+
+    // horizontal
+    if (board[0] === "o" && board[1] === "o" && board[2] === "o") {
+        console.log("O wins!")
+    } else if (board[3] === "o" && board[4] === "o" && board[5] === "o") {
+        console.log("O wins!")
+    } else if (board[6] === "o" && board[7] === "o" && board[8] === "o") {
+        console.log("O wins!")
+    }
+
 }
